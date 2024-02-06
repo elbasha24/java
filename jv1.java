6755ffbd631f5610a8bac2ed64322cfb7922a672
elbasha <elbashaporto@gmail.com>
2024-02-06 22:37:48 +0300

    up

---
+++ jv1.java
@@ -0,0 +1,43 @@
+/**
+ * jv1
+ */
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class jv1 {
+    public static void main(String[] args) {
+      System.out.println("Hello world!");
+
+      int myNum = 5;               // Integer (whole number)
+      float myFloatNum = 5.99f;    // Floating point number
+      char myLetter = 'D';         // Character
+      boolean myBool = true;       // Boolean
+      String myText = "Hello";     // String
+
+      System.out.println(myNum);
+      System.out.println(myFloatNum);
+      System.out.println(myLetter);
+      System.out.println(myBool);
+      System.out.println(myText);
+
+      // int x = 5;
+      // int y = 6;
+      // int z = 50;
+      // System.out.println(x + y + z);
+      // int x = 5, y = 6, z = 50;
+      // System.out.println(x + y + z);
+      // int x, y, z;
+      // x = y = z = 50;
+      // System.out.println(x + y + z);
+      // int myNum = 15;
+      // myNum = 20;  // myNum is now 20
+      // System.out.println(myNum);
+      // final int myNum = 15;
+      // myNum = 20;  // will generate an error: cannot assign a value to a final variable
+      // System.out.println(myNum);
+      // int myNum = 5;               // integer (whole number)
+
+    }
+  }
+    
